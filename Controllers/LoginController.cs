86885d77be1d06b371235102cbdd778222bb6b46
leoquinino <leonardo.quinino@gmail.com>
2023-09-27 00:53:34 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Security.Claims;
+
+namespace WebApplicationApontamentos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new { Msg = "Logado!" });
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logar(string username, string senha)
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection("server=localhost;database=db_apontamentos;uid=root;password=123@Leo");
+            await mySqlConnection.OpenAsync();
+
+            MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = $"SELECT id, nome, username, senha  FROM db_apontamentos.usuarios WHERE username = '{username}' and senha = '{senha}'";
+
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+            if (await reader.ReadAsync())
+            {
+                int usuarioId = reader.GetInt32(0);
+                string nome = reader.GetString(1);
+
+                List<Claim> direitosAcesso = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, usuarioId.ToString()),
+                    new Claim(ClaimTypes.Name, nome)
+                };
+
+                var identity = new ClaimsIdentity(direitosAcesso, "Identity.Login");
+                var userPrincipal = new ClaimsPrincipal(new[] { identity });
+
+                await HttpContext.SignInAsync(userPrincipal,
+                    new AuthenticationProperties
+                    {
+                        IsPersistent = false,                   // SE DESEJA FICAR CONECTADO
+                        ExpiresUtc = DateTime.Now.AddHours(1)   // MANTER A CONEXAO POR ATÉ 1 HORA
+                    });
+
+                return Json(new { Msg = "Logado com sucesso." });
+            }
+
+            return Json(new { Msg = "Verifique dados de Acesso." });
+
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
